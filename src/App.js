25bac75395b253d1454c5912b50580ee048018a8
Shashank Agarwal <shashank1010@gmail.com>
2020-05-31 14:02:35 +0530

    Layouting - Step 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
+
+import HeaderComponent from './components/header/header';
 import './App.scss';
 
 function App() {
   return (
-    <div>Getting Started</div>
+    <Router>
+      <HeaderComponent />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/dashboard" />
+        </Route>
+        <Route path="/dashboard"> </Route>
+        <Route path="/:categoryId"> </Route>
+
+        <Route path="/page-not-found">
+          Page Not found<br />
+          <Link to="/Dash">Go to Dashboard</Link>
+        </Route>
+        <Route path="*" render={
+            ({ location }) =>
+                <Redirect
+                    to={{
+                    pathname: "/page-not-found",
+                    state: { from: location, breadcrumb: false }
+                    }}
+                />
+            }
+        />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/header/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HeaderComponent () {
+    return <header>header</header>
+}
+
+export default HeaderComponent;
